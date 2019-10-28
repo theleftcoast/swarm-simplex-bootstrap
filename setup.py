9aca5f86471a44e8e683f44d36888afd53f7f811
John Towne <towne.john@gmail.com>
2019-10-28 05:08:37 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ssb_optimize",
-    version="0.1.0",
+    version="0.1.2",
     author="John Towne",
     author_email="towne.john@gmail.com",
     description="Particle Swarm and Nelder-Mead Simplex optimization algorithms with Least Squares Regression and "
@@ -14,7 +14,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/theleftcoast/swarm-simplex-bootstrap",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', ],
+    setup_requires=['numpy'],
+    install_requires=['numpy'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
