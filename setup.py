b3e1f0136418d34eb6d498cc534a36730b6d90fb
John Towne <towne.john@gmail.com>
2019-10-27 19:27:04 -0700

    Fixed a bug in depencencies that missed numpy.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/theleftcoast/swarm-simplex-bootstrap",
     packages=setuptools.find_packages(),
+    install_requires=['numpy', ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
